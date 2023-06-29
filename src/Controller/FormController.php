63657684ac372207080daf9f7f7c796d57744d0c
robertlocskai <robertlocskai@gmail.com>
2023-06-29 15:11:59 +0200

    first commit

---
+++ src/Controller/FormController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Message;
+use Twig\Environment;
+use App\Form\ContactFormType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+
+class FormController extends AbstractController
+{
+    ##[Route('/form', name: 'app_form')]
+    #public function index(): Response
+    #{
+    #    return $this->render('form/index.html.twig', [
+    #        'controller_name' => 'FormController',
+    #    ]);
+    #}
+
+    #[Route('/form', name: 'app_form')]
+    public function show(Environment $twig, Request $request, EntityManagerInterface $entityManager) {
+
+        $msg = new Message();
+
+        $form = $this->createForm(ContactFormType::class, $msg);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($msg);
+            $entityManager->flush();
+
+            return new Response("Köszönjük szépen a kérdésedet. Válaszunkkal hamarosan keresünk a megadott e-mail címen.");
+        }
+        else if($form->isSubmitted() && !$form->isValid()) {
+
+            return new Response("Hiba! Kérjük töltsd ki az összes mezőt!");
+
+        }
+
+        return new Response($twig->render('index.html.twig', [
+            'contactForm' => $form->createView()
+        ]));
+
+    }
+
+}
---
+++ src/Form/ContactFormType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Message;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class ContactFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('email', EmailType::class)
+            ->add('messageText')
+            ->add('send', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Message::class,
+            'csrf_protection' => true,
+            'csrf_field_name' => '_token',
+            'csrf_token_id' => 'contact_message'
+        ]);
+    }
+}
